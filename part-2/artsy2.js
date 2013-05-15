ed761ec06c61d316870abf768f640deec5e7730a
Anders Tornblad LBRTW <atornbladlbrtw@gmail.com>
2013-05-15 03:14:33 +0200

    First version of first two effects of second part

--- part-2/artsy2.js
+++ part-2/artsy2.js
@@ -10,9 +10,9 @@
     // Minify, step 1: http://closure-compiler.appspot.com/home
     // Minify, step 2: https://github.com/Siorki/RegPack
     
-    var dev = true;
-    var autoplay = false;
-    var showFrame = true;
+    var dev = false;
+    var autoplay = !dev;
+    var showFrame = false;
     var rnd = Math.random;
     var create = function(elementName) { return document.createElement(elementName); };
     var timeOffset = 0;
@@ -70,6 +70,168 @@
         lastTime, startTime, currentTime, currentChapterIndex = 0, currentRenderer,
         playing, elekfunk, select;
     
+    // *** Endpart loaded scene (renderer 0)
+    // 0.0: Endpart loaded...
+    
+    var muscleMan;
+    
+    var endPartLoadedRenderer = function(subId, chapterOffset, chapterComplete, frameDiff) {
+        if (dev) {
+            if (subId == "getName") {
+                return "Endpart loaded";
+            }
+        }
+        
+        context.fillStyle = "#000000";
+        context.fillRect(0, 0, width, height);
+        
+        var textAlpha = chapterOffset < 1300 ? 0 :
+                        chapterOffset < 2000 ? (chapterOffset - 1300) / 700 :
+                        chapterOffset < 8500 ? 1 :
+                        chapterOffset < 9000 ? (9000 - chapterOffset) / 500 :
+                        0;
+        
+        var imageAlpha = chapterOffset < 2300 ? 0 :
+                         chapterOffset < 3000 ? (chapterOffset - 2300) / 700 :
+                         chapterOffset < 9500 ? 1 :
+                         chapterOffset < 10000 ? (10000 - chapterOffset) / 500 :
+                         0;
+        
+        var color = (textAlpha * 0x77) | 0;
+        var translate = -chapterComplete - 0.75;
+        
+        context.save();
+        context.fillStyle = "rgba(" + color + "," + color + "," + color + ",1)";
+        context.font = "60px sans-serif";
+        context.scale(-0.5, 1);
+        context.translate(width * translate, 0);
+        context.textAlign = "left";
+        context.textBaseline = "top";
+        context.fillText("Endpart loaded...", 0, 50);
+        context.restore();
+        
+        context.globalAlpha = imageAlpha;
+        context.drawImage(muscleMan, halfWidth - muscleMan.width, 460 - muscleMan.height * 2, muscleMan.width * 2, muscleMan.height * 2);
+    };
+    
+    // *** Dotfield scroller scene (renderer 0)
+    // 1.0: Dots and scrolling and wave heights...
+    
+    var dotfieldTextCanvas, dotfieldTextContext;
+    
+    var prepareDotfieldScroller = function() {
+        dotfieldTextCanvas = create("CANVAS");
+        dotfieldTextCanvas.width =
+        dotfieldTextCanvas.height = 50;
+        
+        dotfieldTextContext = dotfieldTextCanvas.getContext("2d");
+    };
+    
+    var dotfieldScrollerRenderer = function(subId, chapterOffset, chapterComplete, frameDiff) {
+        if (dev) {
+            if (subId == "getName") {
+                return "Dotfield scroller";
+            }
+        }
+        
+        context.fillStyle = "#000000";
+        context.fillRect(0, 0, width, height);
+        
+        context.globalAlpha = (chapterComplete < 0.05) ? chapterComplete * 20 :
+                              (chapterComplete > 0.95) ? 20 - chapterComplete * 20 :
+                              1;
+        
+        // 0-7s: Just scroll text, increase speed of offset map to 1.0
+        // 7s:   Rotate full 360 once, decrease speed of offset map to -1.0
+        // 10,6s: Rotate full 360 once
+        // 14,2s: Rotate full 360once, increase speed of offset map to 1.0
+        
+        var angle = chapterOffset < 7000 ? 5000 :
+                    chapterOffset < 8456 ? 5000 + (chapterOffset - 7000) * 45 :
+                    chapterOffset < 10600 ? 5000 :
+                    chapterOffset < 12056 ? 5000 + (chapterOffset - 10600) * 45 :
+                    chapterOffset < 14100 ? 5000 :
+                    chapterOffset < 15556 ? 5000 + (chapterOffset - 14100) * 45 :
+                    5000;
+        
+        var textX = 60 - chapterComplete * 700;
+        
+        dotfieldTextContext.fillStyle = "#001700";
+        dotfieldTextContext.fillRect(0, 0, 50, 50);
+        dotfieldTextContext.textAlign = "left";
+        dotfieldTextContext.textBaseline = "middle";
+        dotfieldTextContext.fillStyle = "#ffffff";
+
+        dotfieldTextContext.font = "bold 40px sans-serif";
+        dotfieldTextContext.fillText("SANITY", textX, 30);
+        dotfieldTextContext.font = "30px sans-serif";
+        dotfieldTextContext.fillText("lbrtw.com for more info!", (textX) + 170, 30);
+
+        dotfieldTextContext.font = "bold 40px sans-serif";
+        dotfieldTextContext.fillText("SANITY", (textX) + 500, 30);
+        
+        var textData = dotfieldTextContext.getImageData(0, 0, 50, 50);
+//        context.drawImage(dotfieldTextCanvas, 0, 20);
+        
+        var textDataOffset = 0;
+        
+        var heightMapOffset = (chapterOffset < 8000) ? smoothComplete(chapterOffset / 8000) :
+                              (chapterOffset < 15000) ? 1 - smoothComplete((chapterOffset - 8000) / 7000) :
+                              smoothComplete((chapterOffset - 15000) / 8000);
+        
+        var heightMapZOffset = heightMapOffset * 160;
+        
+        for (var z = 25; z > -25; --z) {
+            for (var x = -25; x < 25; ++x) {
+                // 1: Rotate (x,y) coordinate around center
+                var rx = x * sinus[(angle + 16384) & 65535] - z * sinus[angle & 65535];
+                var rz = z * sinus[(angle + 16384) & 65535] + x * sinus[angle & 65535];
+                
+                // 2: Tranform into screen coordinates
+                var screenX = halfWidth + rx * 2500 / (300 + rz * 2);
+                var screenY = halfHeight + (10 - rz) * 1200 / (300 + rz * 2);
+                if (z == 0 && x == 0) {
+                    console.log(screenX, screenY);
+                }
+                
+                // 3: Check text bit
+                var isTextBit = textData.data[textDataOffset] > 127;
+                
+                // 4: Get height map height
+                var midXBooster = 1 - (Math.abs(x) / 25);
+                var smallMidXBooster = 0.5 + midXBooster / 2;
+                var offZ = (z + heightMapZOffset) | 0;
+                var highZBooster = offZ / 100;
+                var mapHeight = sinus[((x - 7 + offZ) * 6000) & 65535] * midXBooster +
+                                sinus[(-x * 900) & 65535] +
+                                sinus[((x + 13) * 1300) & 65535] * smallMidXBooster +
+                                sinus[((offZ + 20) * 8000) & 65535] * midXBooster +
+                                sinus[(offZ * 1100) & 65535] +
+                                sinus[((offZ + 10) * 1700) & 65535] * smallMidXBooster;
+                
+                mapHeight = (mapHeight - 2) / 4 + highZBooster * smallMidXBooster;
+                
+                var hasHeight = mapHeight > 0;
+                
+                if (hasHeight && isTextBit) {
+                    context.fillStyle = "rgba(255,130,130,0.65)";
+                    context.fillRect(screenX, (screenY - mapHeight * 50 - 20), 2, 2);
+                } else if (isTextBit) {
+                    context.fillStyle = "rgba(255,27,24,0.67)";
+                    context.fillRect(screenX, (screenY - 20), 2, 2);
+                } else if (hasHeight) {
+                    context.fillStyle = "rgba(255,27,24,0.67)";
+                    context.fillRect(screenX, (screenY - mapHeight * 50), 2, 2);
+                } else {
+                    context.fillStyle = "rgba(80,83,255,0.9)";
+                    context.fillRect(screenX, screenY, 2, 2);
+                }
+                
+                textDataOffset += 4;
+            }
+        }
+    };
+
     // *** Null renderer 
     var nullRenderer = function(subId, chapterOffset, chapterComplete, frameDiff) {
         if (dev) {
@@ -96,14 +258,28 @@
     };
     
     var renderers = [
+            endPartLoadedRenderer,
+            dotfieldScrollerRenderer,
             nullRenderer
         ],
+    
+    nullRendererIndex = 2,
 
     chapters = [
         {
             from : 0,
-            to : 400000,
+            to : 10000,
             rendererIndex : 0,
+            subId : 0
+        }, {
+            from : 10000,
+            to : 29400,
+            rendererIndex : 1,
+            subId : 0
+        }, {
+            from : 29400,
+            to : 201000,
+            rendererIndex : nullRendererIndex,
             subId : 0
         },
         false
@@ -123,6 +299,7 @@
     
     preCalc = function() {
         preCalcSinusTables();
+        prepareDotfieldScroller();
     },
     
     animFrame = function(time) {
@@ -152,6 +329,8 @@
                 context.save();
                 currentRenderer.call(this, currentChapter.subId, chapterTime, chapterComplete, frameDiff);
                 context.restore();
+            } else {
+                playing = false;
             }
             
             var ended = now();
@@ -189,6 +368,11 @@
         
         if (playing) {
             requestAnimationFrame(animFrame);
+        } else {
+            var event = document.createEvent("Event");
+            event.initEvent("demoDone", true, true);
+            
+            window.dispatchEvent(event);
         }
     },
     
@@ -310,7 +494,7 @@
         
         preCalc();
         
-        livininsanity.play();
+        elekfunk.play();
     };
     
     onItemsLoader = function(event) {
@@ -345,6 +529,10 @@
         }
     },
     
+//    onDemoDone = function() {
+//        window.location.href = "/part-3/";
+//    },
+    
     onload = function() {
         canvas = document.getElementById("demo");
         context = canvas.getContext("2d");
@@ -352,23 +540,10 @@
         playing = false;
         
         window.addEventListener("itemsLoader", onItemsLoader, false);
+//        window.addEventListener("demoDone", onDemoDone, false);
         
-        livininsanity = loadAudio("elekfunk.ogg", "elekfunk.mp3");
-        /*whaaaaat = loadImage("whaaaaat.png");
-        biteArte = loadImage("biteArte2.png");
-        deadChicken = loadImage("deadChicken2.png");
-        globe = loadImage("globe.png");
-        globeDoodles = loadImage("globeDoodles.png");
-        sanity1 = loadImage("sanity1.png");
-        madman = loadImage("madman.png");
-        tunnelBitmap = loadImage("tunnelBits.png");
-        tunnelWall = loadImage("tunnelWall.png");
-        tunnelHanger = loadImage("tunnelHanger.png");
-        einstein = loadImage("einstein.png");
-        sanity2 = loadImage("sanity2.png");
-        buddha = loadImage("seventies.png");
-        hailSalvadore = loadImage("hailSalvadore.png");
-        trueArtists = loadImage("trueArtists.png");*/
+        elekfunk = loadAudio("elekfunk.ogg", "elekfunk.mp3");
+        muscleMan = loadImage("muscleMan.png");
         
         if (dev) {
             select = document.createElement("SELECT");
