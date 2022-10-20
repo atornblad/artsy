d9020dc84a9320fdd301c591554fa8dd7c84e7f2
Anders Marzi Tornblad <anders.tornblad@gmail.com>
2022-10-20 22:04:52 +0200

    Added vibrato effect

--- tracker-worklet.js
+++ tracker-worklet.js
@@ -1,5 +1,8 @@
 const debuggedEffects = {};
 
+const HALF_NOTE_STEP = Math.pow(2, 1 / 12);
+const halfNoteSteps = (halfNotes) => Math.pow(2, halfNotes / 12);
+
 class TrackerWorklet extends AudioWorkletProcessor {
     constructor(options) {
         super(options);
@@ -12,25 +15,29 @@ class TrackerWorklet extends AudioWorkletProcessor {
             at: 0,
             period: 0,
             effect: 0,
-            volume: 0
+            volume: 0,
+            vibrato: { notes : 0, speed : 0, active : true }
         }, {
             instr: null,
             at: 0,
             period: 0,
             effect: 0,
-            volume: 0
+            volume: 0,
+            vibrato: { notes : 0, speed : 0, active : true }
         }, {
             instr: null,
             at: 0,
             period: 0,
             effect: 0,
-            volume: 0
+            volume: 0,
+            vibrato: { notes : 0, speed : 0, active : true }
         }, {
             instr: null,
             at: 0,
             period: 0,
             effect: 0,
-            volume: 0
+            volume: 0,
+            vibrato: { notes : 0, speed : 0, active : true }
         } ];
         this.port.onmessage = this.onmessage.bind(this);
     }
@@ -70,24 +77,35 @@ class TrackerWorklet extends AudioWorkletProcessor {
         }
 
         for (let i = 0; i < 4; ++i) {
+            const ch = this.channels[i];
             if (row[i].instr) {
                 const instr = this.data.instruments[row[i].instr - 1];
-                this.channels[i].instr = instr;
-                this.channels[i].at = 0;
-                this.channels[i].volume = Math.max(0, Math.min(1, instr.volume / 64));
+                ch.instr = instr;
+                ch.at = 0;
+                ch.volume = Math.max(0, Math.min(1, instr.volume / 64));
             }
 
             if (row[i].period) {
-                this.channels[i].period = row[i].period;
+                ch.period = row[i].period;
             }
 
+            ch.vibrato.active = false;
             if (row[i].effect) {
                 const command = row[i].effect >> 8;
                 const data = row[i].effect & 0xff;
+                const upper = (data & 0xf0) >> 4;
+                const lower = data & 0x0f;
                 switch (command) {
+                    case 0x4:
+                        // 4: Vibrato
+                        ch.vibrato.active = true;
+                        if (upper) ch.vibrato.notes = lower;
+                        if (lower) ch.vibrato.speed = upper;
+                        console.log(ch.vibrato);
+                        break;
                     case 0x9:
                         // 9: Set sample offset
-                        this.channels[i].at = data * 256;
+                        ch.at = data * 256;
                         break;
                     case 0xb:
                         // B: Position Jump
@@ -96,11 +114,11 @@ class TrackerWorklet extends AudioWorkletProcessor {
                         break;
                     case 0xc:
                         // C: Set volume
-                        this.channels[i].volume = Math.max(0, Math.min(1, data / 64))
+                        ch.volume = Math.max(0, Math.min(1, data / 64))
                         break;
                     case 0xd:
                         // D: Pattern Break
-                        this.nextRowIndex = ((data & 0xf0) >> 4) * 10 + (data & 0x0f);
+                        this.nextRowIndex = (upper >> 4) * 10 + lower;
                         this.nextSongPos++;
                         break;
                     case 0xf:
@@ -134,7 +152,12 @@ class TrackerWorklet extends AudioWorkletProcessor {
                 if (!ch) continue;
                 if (!ch.instr) continue;
                 value += ch.instr.samples[ch.at | 0] * ch.volume / 200;
-                ch.at += this.rate / (ch.period - ch.instr.fineTune);
+                let atStep = this.rate / (ch.period - ch.instr.fineTune);
+                if (ch.vibrato.active) {
+                    // Not completely sure about this, but it sounds okay for now.
+                    atStep *= (1 + (Math.sin(this.time * ch.vibrato.speed * this.bpm * 6) * (halfNoteSteps(ch.vibrato.notes / 8) - 1)));
+                }
+                ch.at += atStep;
                 if (ch.instr.repeat) {
                     if (ch.at >= ch.instr.repeat.start + ch.instr.repeat.length) {
                         ch.at -= ch.instr.repeat.length;
@@ -147,6 +170,7 @@ class TrackerWorklet extends AudioWorkletProcessor {
             channel[i] = Math.tanh(value);
         }
         this.debug = false;
+        this.time += 1 / this.sampleRate;
         return true;
     }
 }   
