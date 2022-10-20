34932111544b235fc8f560952675b9bea0464f3b
Anders Marzi Tornblad <anders.tornblad@gmail.com>
2022-10-20 21:41:20 +0200

    Better playback, started on some effects

--- tracker-worklet.js
+++ tracker-worklet.js
@@ -1,3 +1,4 @@
+const debuggedEffects = {};
 
 class TrackerWorklet extends AudioWorkletProcessor {
     constructor(options) {
@@ -6,7 +7,31 @@ class TrackerWorklet extends AudioWorkletProcessor {
 
         this.time = 0;
         this.debug = true;
-        this.channels = [ null, null, null, null ];
+        this.channels = [ {
+            instr: null,
+            at: 0,
+            period: 0,
+            effect: 0,
+            volume: 0
+        }, {
+            instr: null,
+            at: 0,
+            period: 0,
+            effect: 0,
+            volume: 0
+        }, {
+            instr: null,
+            at: 0,
+            period: 0,
+            effect: 0,
+            volume: 0
+        }, {
+            instr: null,
+            at: 0,
+            period: 0,
+            effect: 0,
+            volume: 0
+        } ];
         this.port.onmessage = this.onmessage.bind(this);
     }
 
@@ -18,12 +43,22 @@ class TrackerWorklet extends AudioWorkletProcessor {
         this.nextSongPos = 0;
         this.nextRowIndex = 0;
         this.bpm = 125;
-        this.samplesPerRow = (this.sampleRate * 60) / (this.bpm * 4);
-        this.nextRowAfter = 0;
+        this.samplesPerTick = (this.sampleRate * 60) / (this.bpm * 4) / 6;
+        this.nextTickAfter = 0;
+        this.tick = 0;
+        this.ticksPerRow = 6;
+    }
+
+    nextTick() {
+        ++this.tick;
+        if (this.tick >= this.ticksPerRow) {
+            this.tick = 0;
+            this.nextRow();
+        }
+        this.nextTickAfter += this.samplesPerTick;
     }
 
     nextRow() {
-        this.nextRowAfter += this.samplesPerRow;
         const patternIndex = this.data.patternIndices[this.nextSongPos];
         const pattern = this.data.patterns[patternIndex];
         const row = pattern[this.nextRowIndex];
@@ -37,17 +72,48 @@ class TrackerWorklet extends AudioWorkletProcessor {
         for (let i = 0; i < 4; ++i) {
             if (row[i].instr) {
                 const instr = this.data.instruments[row[i].instr - 1];
-
-                this.channels[i] = {
-                    instr: instr,
-                    at: 0,
-                    period: row[i].period,
-                    effect: row[i].effectCommand,
-                    volume: Math.max(0, Math.min(1, instr.volume / 64))
-                };
+                this.channels[i].instr = instr;
+                this.channels[i].at = 0;
+                this.channels[i].volume = Math.max(0, Math.min(1, instr.volume / 64));
             }
-            else {
-                //this.channels[i] = null;
+
+            if (row[i].period) {
+                this.channels[i].period = row[i].period;
+            }
+
+            if (row[i].effect) {
+                const command = row[i].effect >> 8;
+                const data = row[i].effect & 0xff;
+                switch (command) {
+                    case 0x9:
+                        // 9: Set sample offset
+                        this.channels[i].at = data * 256;
+                        break;
+                    case 0xb:
+                        // B: Position Jump
+                        this.nextRowIndex = 0;
+                        this.nextSongPos = data & 0x7f;
+                        break;
+                    case 0xc:
+                        // C: Set volume
+                        this.channels[i].volume = Math.max(0, Math.min(1, data / 64))
+                        break;
+                    case 0xd:
+                        // D: Pattern Break
+                        this.nextRowIndex = ((data & 0xf0) >> 4) * 10 + (data & 0x0f);
+                        this.nextSongPos++;
+                        break;
+                    case 0xf:
+                        // F: Set speed
+                        this.ticksPerRow = data;
+                        break;
+                    default:
+                        const key = command.toString(16);
+                        if (!(key in debuggedEffects)) {
+                            debuggedEffects[key] = true;
+                            console.log(`Unhandled effect ${key}`);
+                        }
+                }
             }
         }
     }
@@ -57,24 +123,25 @@ class TrackerWorklet extends AudioWorkletProcessor {
         const channel = output[0];
 
         for (let i = 0; i < channel.length; ++i) {
-            if (this.nextRowAfter <= 0) {
-                this.nextRow();
+            if (this.nextTickAfter <= 0) {
+                this.nextTick();
             }
-            this.nextRowAfter--;
+            this.nextTickAfter--;
             let value = 0.0;
 
             for (let c = 0; c < 4; ++c) {
                 const ch = this.channels[c];
                 if (!ch) continue;
+                if (!ch.instr) continue;
                 value += ch.instr.samples[ch.at | 0] * ch.volume / 200;
-                ch.at += this.rate / (ch.period + ch.instr.fineTune);
+                ch.at += this.rate / (ch.period - ch.instr.fineTune);
                 if (ch.instr.repeat) {
                     if (ch.at >= ch.instr.repeat.start + ch.instr.repeat.length) {
                         ch.at -= ch.instr.repeat.length;
                     }
                 }
                 else if (ch.at >= ch.instr.length) {
-                    this.channels[i] = null;
+                    ch.instr = null;
                 }
             }
             channel[i] = Math.tanh(value);
